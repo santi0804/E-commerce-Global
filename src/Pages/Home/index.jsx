37c2e19f17599048b4b768dfe3a86d1ab3d474ca
santi0804 <santama.1988@gmail.com>
2024-12-03 14:16:49 -0500

    Consumo de API para los productos

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,14 +1,30 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
 
 
 function Home() {
+
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+    }, [])
 
     return (
 
         <Layout>
             Home
+            <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg"> {/*Con esta linea mostramos los productos en columnas diferentes */}
+                {
+                    items?.map(items => (
+                        <Card key={items.id} data={items} />   // Asi renderizamos nuestras cards
+                    ))                         // y traemos la peticion de la data con los items desde el componente Card.
+                }
+            </div>
         </Layout>
-
     )
 }
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,8 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
