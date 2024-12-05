33690667b8a77c6d991516098610866d17e250f9
santi0804 <santama.1988@gmail.com>
2024-12-05 17:00:38 -0500

    Funcionalidad el componente ProductDetail

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -6,7 +6,9 @@ const Card = (data) => {
     const context = useContext(ShoppingCarContext)  // este es el estado global
 
     return (
-        <div className="bg-white cursor-pointer w-56 h-60"> {/*Estilo de Tailwinds para el container principal */}
+        <div 
+        className="bg-white cursor-pointer w-56 h-60"
+        onClick={() => context.openProductDetail()} > {/*Este cambia el estado*/}
             <figure className="relative mb-2 w-full h-4/5 ">
                 <span className="absolute bottom-0 left-0 bg-white/60 round-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category.name}</span>
                 <img className="w-full h-full object-cover rounded-lg" src={data.data.images[0]} alt={data.data.title} />  {/*Con esta linea podemos traer las imagenes para cada card diferente */}
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,12 +4,18 @@ export const ShoppingCarContext = createContext()
 
 export const ShoppinCartProvider = ({ children }) => {
     const [count, setCount] = useState(0);
-
+    const [isProductDetailOpen, setisProductDetailOpen] = useState(false);
+    const openProductDetail = () => setisProductDetailOpen(true);
+    const closeProductDetail = () => setisProductDetailOpen(false);
+    
 
     return (
         <ShoppingCarContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen
         }}>
             {children}
         </ShoppingCarContext.Provider>
